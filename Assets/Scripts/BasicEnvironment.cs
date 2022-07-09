e3f5574d132c5a551a83b97b3fbda99ea53c89b3
GitLime <Emilius92@hotmail.com>
2022-07-09 21:03:54 +0200

    Environment builder adding coins to scene

---
+++ Assets/Scripts/BasicEnvironment.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnvironment : MonoBehaviour
+{
+
+    public GameObject rewardPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        generateCoins();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void generateCoins()
+    {
+
+        float coinHeight = 0.1F;
+
+        for (int x = 0; x < 10; x++)
+        {
+            for (int z = 0; z < 10; z++)
+            {
+                Vector3 position = new Vector3(5+x*10,coinHeight,5+z*10);
+                Instantiate(rewardPrefab, position, new Quaternion());
+                coinHeight += 0.1F;
+            }
+        }
+    }
+}
